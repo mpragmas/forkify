23d059d4493e0c749cf8c3899b3e1866211ae494
mpragmas <mpragmas@gmail.com>
2024-04-08 17:42:39 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('welcome to the application');
+};
 
 const init = function () {
   bookmarkView.addHandlerRender(controlBookmarks);
@@ -133,6 +136,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('welcome');
+  newFeature();
 };
 init();
